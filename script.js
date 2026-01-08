25a9cc09c25911d03abc69c9bfee519b9f64a020
presitana-maker <presitana@gmail.com>
2026-01-08 09:56:49 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let func=()=>
+{
+    let num1=Number(document.getElementById('num1').value)
+    let num2=Number(document.getElementById('num2').value)
+   let newele= document.createElement("a")
+
+    let res=document.getElementById('para')
+    res.innerHTML=(num1+num2)
+    newele.innerHTML='ghkdkk'
+    document.body.insertAdjacentElement(num1,newele)
+    
+}
+
+
+
+
+
+
+
